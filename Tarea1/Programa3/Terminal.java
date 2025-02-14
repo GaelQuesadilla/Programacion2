b49961b72eaa6c629eee32ba8f60c4f324d6ac1b
Gael González <gaeldev032@gmail.com>
2025-02-14 16:37:54 -0600

    Tarea1 Programa 3

---
+++ Tarea1/Programa3/Terminal.java
@@ -0,0 +1,18 @@
+package Tarea1.Programa3;
+
+import java.util.Scanner;
+
+public class Terminal {
+
+    public int getVote(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        int value = scanner.nextInt();
+        return value;
+    }
+
+    public void Print(String message) {
+        System.out.println(message);
+    }
+
+}
