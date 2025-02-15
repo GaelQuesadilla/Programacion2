c9af4be7aad19ff1bf2565afae7f4f56d6e3053a
Gael González <gaeldev032@gmail.com>
2025-02-14 18:22:09 -0600

    Tarea 2 Programa 6

---
+++ Tarea2/Programa6/Terminal.java
@@ -0,0 +1,32 @@
+package Tarea2.Programa6;
+
+import java.util.Scanner;
+
+public class Terminal {
+
+    public int getInt(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        int value = scanner.nextInt();
+        return value;
+    }
+
+    public double getDouble(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        double value = scanner.nextDouble();
+        return value;
+    }
+
+    public String getString(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        String value = scanner.nextLine();
+        return value;
+    }
+
+    public void Print(String message) {
+        System.out.println(message);
+    }
+
+}
