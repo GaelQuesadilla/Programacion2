48a55253e94d8502529ae96c77ced0f95853e97b
Gael Gonzalez <gaelglz032@gmail.com>
2025-02-25 18:33:08 -0600

    Avances

---
+++ Tarea5/Programa16/Functions.java
@@ -0,0 +1,40 @@
+package Tarea5.Programa16;
+
+public class Functions {
+
+    private int result, number;
+
+    private int digitSum(int number) {
+        if (number == 0) {
+            return 0;
+
+        }
+
+        return number % 10 + digitSum(number / 10);
+    }
+
+    public void calcDigitSum() {
+        this.result = digitSum(number);
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+
+    }
+
+    public Functions(int number) {
+        this.number = number;
+    }
+
+    public Functions() {
+    }
+
+}
