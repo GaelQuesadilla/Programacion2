b26a3fdd72ca4831efcca5d828933c0d4971ed39
Gael González <gaeldev032@gmail.com>
2025-03-06 20:15:11 -0600

    Añadiendo tarea 8

---
+++ Tarea8/Programa22/InsertionSort.java
@@ -0,0 +1,48 @@
+package Tarea8.Programa22;
+
+public class InsertionSort {
+
+    private double[] array;
+    private double average;
+
+    public void sort() {
+        double index;
+        for (int i = 1; i < array.length; i++) {
+            index = array[i];
+            int a = i - 1;
+            while (a >= 0 && array[a] > index) {
+                array[a + 1] = array[a];
+                a = a - 1;
+            }
+            array[a + 1] = index;
+        }
+    }
+
+    public void calcAverage() {
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        average = sum / array.length;
+    }
+
+    public double[] getArray() {
+        return array;
+    }
+
+    public void setArray(double[] array) {
+        this.array = array;
+    }
+
+    public InsertionSort(double[] array) {
+        this.array = array;
+    }
+
+    public InsertionSort() {
+    }
+
+    public double getAverage() {
+        return average;
+    }
+
+}
---
+++ Tarea8/Programa22/Main.java
@@ -0,0 +1,38 @@
+package Tarea8.Programa22;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int total = 15;
+        Terminal terminal = new Terminal(new Scanner(System.in));
+        InsertionSort insertionSort = new InsertionSort();
+        double[] sales = new double[total];
+
+        terminal.Print("Equipo 7. Elaborado por:");
+        terminal.Print("Frida Alejandra Mendoza Martinez");
+        terminal.Print("Gael González Méndez");
+        terminal.Print("Héctor Desiderio Erasto");
+
+        terminal.Print(
+                "Registra las ventas diarias registradas durante 15 días, calcula el promedio de las ventas y muestra la mediana");
+
+        for (int i = 0; i < total; i++) {
+            sales[i] = terminal.requestInt("Ingresa las ventas del día " + (i + 1));
+        }
+
+        insertionSort.setArray(sales);
+
+        terminal.Print("Ventas sin ordenar");
+        terminal.Print(sales);
+
+        insertionSort.sort();
+        terminal.Print("Ventas ordenadas");
+        terminal.Print(insertionSort.getArray());
+
+        insertionSort.calcAverage();
+        terminal.Print("El promedio de las ventas es: " + insertionSort.getAverage());
+        terminal.Print("La mediana de las ventas es: " + insertionSort.getArray()[total / 2]); // array[7]
+
+    }
+}
